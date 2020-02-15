ed0bcbc8ffb5d327bb4b375e4b349ea60d9a4088
Varun Vishal <varunvishal10@gmail.com>
2020-02-16 01:20:57 +0530

    Updates

---
+++ precache-manifest.9e541c182481d77091289f4eed71ea27.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03b56491061627246ea1bcc51d5706f",
+    "url": "/inceptra/index.html"
+  },
+  {
+    "revision": "025a515a7e9f547a50d0",
+    "url": "/inceptra/static/css/2.8e1586cd.chunk.css"
+  },
+  {
+    "revision": "bb1623f183f19138f6ac",
+    "url": "/inceptra/static/css/main.bc8b573d.chunk.css"
+  },
+  {
+    "revision": "025a515a7e9f547a50d0",
+    "url": "/inceptra/static/js/2.93de9a62.chunk.js"
+  },
+  {
+    "revision": "bd85c3b1623efb25c2202260e8c02aee",
+    "url": "/inceptra/static/js/2.93de9a62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb1623f183f19138f6ac",
+    "url": "/inceptra/static/js/main.95da3881.chunk.js"
+  },
+  {
+    "revision": "4f62235fbbb61ea2543e",
+    "url": "/inceptra/static/js/runtime-main.b14ea5a1.js"
+  },
+  {
+    "revision": "39f61212e9f2d6ad7c1bb8f068627aa3",
+    "url": "/inceptra/static/media/SB.39f61212.jpg"
+  },
+  {
+    "revision": "276828757edeef8ee71b8aa67cbdbed1",
+    "url": "/inceptra/static/media/SM.27682875.jpg"
+  },
+  {
+    "revision": "f7b5f9531cd363f592b336535665ca1b",
+    "url": "/inceptra/static/media/abhishek.f7b5f953.jpg"
+  },
+  {
+    "revision": "718cbda883c3a78727b77a59a5725df6",
+    "url": "/inceptra/static/media/alebhia.718cbda8.jpg"
+  },
+  {
+    "revision": "14cc6c2d177c9db53c14352146536c40",
+    "url": "/inceptra/static/media/alekh.14cc6c2d.jpg"
+  },
+  {
+    "revision": "cb88478c1cd1bce15c5c79a28deb8149",
+    "url": "/inceptra/static/media/anuska.cb88478c.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/inceptra/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/inceptra/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "c08432d9e1b9f37c363fef9fd608d324",
+    "url": "/inceptra/static/media/band.c08432d9.png"
+  },
+  {
+    "revision": "4a6d021d88e9c72c7722d6276115a2d6",
+    "url": "/inceptra/static/media/bot.4a6d021d.png"
+  },
+  {
+    "revision": "3ed4c97528bb57e10a645b2d5c44aea3",
+    "url": "/inceptra/static/media/bulb.3ed4c975.png"
+  },
+  {
+    "revision": "e93d8f0d04fb6c6e84769da3962a9eeb",
+    "url": "/inceptra/static/media/cb.e93d8f0d.jpg"
+  },
+  {
+    "revision": "ed3465907c5da262d5116463a782a814",
+    "url": "/inceptra/static/media/cl.ed346590.gif"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/inceptra/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/inceptra/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/inceptra/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/inceptra/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/inceptra/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/inceptra/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/inceptra/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/inceptra/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/inceptra/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/inceptra/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/inceptra/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/inceptra/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/inceptra/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/inceptra/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/inceptra/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "f9ec10056a996801e6c0fb785dc0a881",
+    "url": "/inceptra/static/media/indianoil.f9ec1005.png"
+  },
+  {
+    "revision": "3e6841bf9929173ceaec84fa63dc0424",
+    "url": "/inceptra/static/media/jamboree.3e6841bf.jpg"
+  },
+  {
+    "revision": "dcad7bb47bfc03b4154a7c7c9688bc20",
+    "url": "/inceptra/static/media/kaustav.dcad7bb4.jpg"
+  },
+  {
+    "revision": "e420cb2dbfccae24e51cd8afcb6bf7ba",
+    "url": "/inceptra/static/media/manya.e420cb2d.png"
+  },
+  {
+    "revision": "994cdfa2ac5aa973e80441ce58e4bfe2",
+    "url": "/inceptra/static/media/music.994cdfa2.png"
+  },
+  {
+    "revision": "a0724f68a6b1715b57dccc63a36fec25",
+    "url": "/inceptra/static/media/nilesh.a0724f68.jpeg"
+  },
+  {
+    "revision": "be40b258b9838463e74dcf412f065bde",
+    "url": "/inceptra/static/media/ritesh.be40b258.jpg"
+  },
+  {
+    "revision": "b9421c14077b7fdcecb228c63646f35d",
+    "url": "/inceptra/static/media/satish.b9421c14.jpg"
+  },
+  {
+    "revision": "01224fb98f0311763cda3cf657bdc81a",
+    "url": "/inceptra/static/media/singer.01224fb9.png"
+  },
+  {
+    "revision": "b8766048718df97f66bc7a5dbcf02c7b",
+    "url": "/inceptra/static/media/ss.b8766048.jpg"
+  },
+  {
+    "revision": "e7fcdef481a4c2d34890b16b952f7bda",
+    "url": "/inceptra/static/media/stage.e7fcdef4.png"
+  },
+  {
+    "revision": "5f05bde1a49c73cc4d8b2fee9ab209c9",
+    "url": "/inceptra/static/media/subham.5f05bde1.jpg"
+  },
+  {
+    "revision": "b0dca1538a110dd5ee860db55e6ea2e7",
+    "url": "/inceptra/static/media/team.b0dca153.png"
+  },
+  {
+    "revision": "588172f803c9efb32675df95f521e910",
+    "url": "/inceptra/static/media/toyota.588172f8.png"
+  },
+  {
+    "revision": "9c50b48fcbe03edf9a93a7178d75d971",
+    "url": "/inceptra/static/media/tpr.9c50b48f.png"
+  },
+  {
+    "revision": "20fff112519d1cfadaa30d529dae45d1",
+    "url": "/inceptra/static/media/varun.20fff112.jpeg"
+  },
+  {
+    "revision": "c162c6f97ea3b6bd640e00b5d1d342cc",
+    "url": "/inceptra/static/media/vishal.c162c6f9.jpg"
+  },
+  {
+    "revision": "cf0b4e3ffd21f34ff81e1bd6437d1e77",
+    "url": "/inceptra/static/media/vishwajeet.cf0b4e3f.jpg"
+  }
+]);
